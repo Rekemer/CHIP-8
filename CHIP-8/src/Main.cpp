cf53f3811bee54d050477e30f42c5435c532a3cc
Ilia <ilya.barselona@gmail.com>
2024-03-07 14:46:22 +0200

    add display functions

--- CHIP-8/src/Main.cpp
+++ CHIP-8/src/Main.cpp
@@ -5,15 +5,26 @@
 #include<cassert>
 #include <time.h>
 #include <algorithm>
+#define GLUT_STATIC_LIB
+#include "glut.h"
 using Byte = uint8_t;
 using Word = uint16_t;
 Byte delayTimer = 0;
 Byte soundTimer = 0;
+const int SCREEN_W = 64;
+const int SCREEN_H= 32;
+
+int modifier = 10;
+
+// Window size
+int display_width = SCREEN_W * modifier;
+int display_height = SCREEN_H * modifier;
+
 constexpr size_t MEMORY_SIZE= 4096;
 std::array<Byte, MEMORY_SIZE> m_Memory;
 std::array<Byte, 16> m_VRegisters;
 // must be 12 bits register
-Word m_AddressI;
+Word m_AddressI= 0;
 Word m_ProgramCounter; // the 16-bit program counter
 std::array<Word,16> m_Stack; // to save programm coutner value if subroutine is called
 int m_StackPtr = 0;
@@ -39,13 +50,13 @@ unsigned char Fontset[80] =
 };
 constexpr int ScreenDataSize = 64 * 32;
 std::array<Byte, ScreenDataSize> m_ScreenData;
-std::array<unsigned char, 16>key;
+std::array<unsigned char, 16>m_Keys;
 // two bytes make up opCode, so programm counter increments by 2
 Word GetOpCode(Byte a, Byte b)
 {
 	Word res = 0;
 	res = a;
-	res = res << 8;
+	res = a << 8;
 	res |= b;
 	return res;
 }
@@ -62,13 +73,23 @@ void ExecuteOpCode(Word opCode)
 		switch (opCode & 0x0FFF)
 		{
 		case 0x00E0: // clears the screen
+		{
+
 			m_UpdateScreen = true;
 			m_ScreenData.fill(0);
+			m_ProgramCounter += 2;
 			break;
+		}
 		case 0x00EE: // returns from subroutine
+		{
+
+			m_StackPtr--;
 			m_ProgramCounter = m_Stack[m_StackPtr];
+			m_ProgramCounter+=2;
 			break;
+		}
 		default:
+			printf("Unknown opcode [0x0000]: 0x%X\n", opCode);
 			break;
 		}
 		break;
@@ -131,43 +152,63 @@ void ExecuteOpCode(Word opCode)
 	{
 		Word x = (opCode >> 8) & 0x000F;
 		Word nn = opCode  & 0x00FF;
-		m_VRegisters[x] = nn; m_ProgramCounter += 2;
+		m_VRegisters[x] = nn; 
+		m_ProgramCounter += 2;
 	}
 	break;
 	case 0x7000:// 0x7XNN Adds NN to VX
 	{
 		Word x = (opCode >> 8) & 0x000F;
 		Word nn = opCode & 0x00FF;
-		m_VRegisters[x] += nn; m_ProgramCounter += 2;
+		m_VRegisters[x] += nn; 
+		m_ProgramCounter += 2;
 	}
 		break;
 	case 0x8000: 
 		switch (opCode & 0x000F)
 		{
 		case 0x0000: // 0x8XY0: sets VX to VY 
-			Word x = (opCode << 8) & 0x000F;
-			Word y = (opCode << 4) & 0x000F;
+		{
+			Word x = (opCode >> 8) & 0x000F;
+			Word y = (opCode >> 4) & 0x000F;
 			m_VRegisters[x] = m_VRegisters[y];
+			m_ProgramCounter += 2;
+
 			break;
+		}
 		case 0x0001: // 0x8XY1: sets VX to VY | VX
-			Word x = (opCode << 8) & 0x000F;
-			Word y = (opCode << 4) & 0x000F;
-			m_VRegisters[x] |= m_VRegisters[y]; m_ProgramCounter += 2;
+		{
+
+			Word x = (opCode >> 8) & 0x000F;
+			Word y = (opCode >> 4) & 0x000F;
+			m_VRegisters[x] |= m_VRegisters[y];
+			m_ProgramCounter += 2;
 			break;
+		}
 		case 0x0002: // 0x8XY2: sets VX to VY & VX	
-			Word x = (opCode << 8) & 0x000F;
-			Word y = (opCode << 4) & 0x000F;
-			m_VRegisters[x] &= m_VRegisters[y]; m_ProgramCounter += 2;
+
+		{
+			Word x = (opCode >> 8) & 0x000F;
+			Word y = (opCode >> 4) & 0x000F;
+			m_VRegisters[x] &= m_VRegisters[y];
+			m_ProgramCounter += 2;
 			break;
+
+		}
 		case 0x0003: // 0x8XY3: sets VX to VY xor VX
-			Word x = (opCode << 8) & 0x000F;
-			Word y = (opCode << 4) & 0x000F;
+		{
+
+			Word x = (opCode >> 8) & 0x000F;
+			Word y = (opCode >> 4) & 0x000F;
 			m_VRegisters[x] ^= m_VRegisters[y];
 			m_ProgramCounter += 2;
 			break;
+		}
 		case 0x0004: // 0x8XY4: adds VY to VX. VF is set to 1 when there's an overflow,  and to 0 when there is not
-			Word x = (opCode << 8) & 0x000F;
-			Word y = (opCode << 4) & 0x000F;
+
+		{
+			Word x = (opCode >> 8) & 0x000F;
+			Word y = (opCode >> 4) & 0x000F;
 			// a + b <= max
 			// a <= max - b if that is not the case then we have overflow situation
 			bool isOverflow = m_VRegisters[x] > (0xFF - m_VRegisters[y]);
@@ -175,13 +216,17 @@ void ExecuteOpCode(Word opCode)
 			m_VRegisters[x] += m_VRegisters[y];
 			m_ProgramCounter += 2;
 			break;	  
+		}
 					 
 
 		case 0x0005: // 0x8XY5: substract VY from VX
 					 // VF is set to 0 when there's an overflow,
 					// and to 1 when there is not
-			Word x = (opCode << 8) & 0x000F;
-			Word y = (opCode << 4) & 0x000F;
+
+		{
+
+			Word x = (opCode >> 8) & 0x000F;
+			Word y = (opCode >> 4) & 0x000F;
 			// a - b >= min
 			// a  >= min - b if that is not the case then we have overflow situation
 			bool isUnderflow = m_VRegisters[x] < m_VRegisters[y];
@@ -189,39 +234,54 @@ void ExecuteOpCode(Word opCode)
 			m_VRegisters[x] -= m_VRegisters[y];
 			m_ProgramCounter += 2;
 			break;		 
+		}
+
 		case 0x0006: // 0x8XY6: stores the least significant bit of VX in VF
 					 // and then shifts VX to right by 1
-			Word x = (opCode << 8) & 0x000F;
+
+		{
+			Word x = (opCode >> 8) & 0x000F;
 			m_VRegisters[0xF] = m_VRegisters[x] & 0x1;
 			m_VRegisters[x] >>= 1;
 			m_ProgramCounter += 2;
 			break;
 
+		}
+
 		case 0x0007: // 0x8XY7: Sets VX to VY minus VX.
 					 // VF is set to 0 when there's an underflow, 
 					 // and 1 when there is not
-			Word x = (opCode << 8) & 0x000F;
-			Word y = (opCode << 4) & 0x000F;
+		{
+
+			Word x = (opCode >> 8) & 0x000F;
+			Word y = (opCode >> 4) & 0x000F;
 			bool isUnderflow = m_VRegisters[x] < m_VRegisters[y];
 			m_VRegisters[0xF] = (Byte)!isUnderflow;
 			m_VRegisters[x] = m_VRegisters[y] - m_VRegisters[x];
 			m_ProgramCounter += 2;
 			break;
+		}
 		case 0x000E: // 0x8XYE: Stores the most significant bit of VX in VF 
 					 // and then shifts VX to the left by 1
-			Word x = (opCode << 8) & 0x000F;
+		{
+
+			Word x = (opCode >> 8) & 0x000F;
 			m_VRegisters[0xF] = (m_VRegisters[x] << 7 ) & 0x1;
 			m_VRegisters[x] <<= 1;
 			m_ProgramCounter += 2;
 			break;
-		default:	
+		}
+		default:
+			printf("Unknown opcode [0x0000]: 0x%X\n", opCode);
 			break;	
 		}
 		break;
 	case 0x9000:// 0x9XY0: Skips the next instruction if VX does not equal VY.
 
-		Word x = (opCode << 8) & 0x000F;
-		Word y = (opCode << 4) & 0x000F;
+	{
+
+		Word x = (opCode >> 8) & 0x000F;
+		Word y = (opCode >> 4) & 0x000F;
 		bool check = m_VRegisters[x] == m_VRegisters[y];
 		if (check)
 		{
@@ -232,23 +292,33 @@ void ExecuteOpCode(Word opCode)
 			m_ProgramCounter += 2;
 		}
 		break;
+	}
 	case 0xA000: // 0xANNN: Sets I to the address NNN
-		Word nnn = opCode & 0x0FFF;
-		m_AddressI = nnn;
-		m_ProgramCounter += 2;
-		break;
+	{
+
+			Word nnn = opCode & 0x0FFF;
+			m_AddressI = nnn;
+			m_ProgramCounter += 2;
+			break;
+	}
 	case 0xB000: // 0xBNNN: Jumps to the address NNN plus V0
+	{
+
 		Word nnn = opCode & 0x0FFF;
 		m_ProgramCounter = nnn + m_VRegisters[0];
 		m_ProgramCounter += 2;
 		break;
+	}
 	case 0xC000: // 0xCXNN: Sets VX to the result of a bitwise and operation on a random number (Typically: 0 to 255) and NN
+		
+	{
 		auto rand = Random();
-		Word x = (opCode << 8) & 0x000F;
+		Word x = (opCode >> 8) & 0x000F;
 		Word nn = (opCode) & 0x00FF;
 		m_VRegisters[x] = rand & nn;
 		m_ProgramCounter += 2;
 		break;
+	}	
 	case 0xD000:
 		/*
 		 0xDXYN:
@@ -258,13 +328,38 @@ void ExecuteOpCode(Word opCode)
 			As described above, VF is set to 1 if any screen pixels are flipped from set to unset when the sprite is drawn, 
 			and to 0 if that does not happen.
 		*/
+
+	{
+
+		Word x = (opCode >> 8) & 0x000F;
+		Word y = (opCode >> 4) & 0x000F;
+		Word height = (opCode ) & 0x000F;
+		const Word width = 8;
+		for (int i = 0; i < height; i++)
+		{
+			Word pixel = m_Memory[m_AddressI + i];
+			for (int j = 0; j < width; j++)
+			{
+				if ((pixel & (0x80 >> j)) != 0)
+				{
+				  if (m_ScreenData[(x + j + ((y + i) * 64))] == 1)
+					  m_VRegisters[0xF] = 1;
+				  m_ScreenData[x + j + ((y + i) * 64)] ^= 1;
+				}
+			}
+		}
+		m_ProgramCounter += 2;
+		m_UpdateScreen = true;
 		break;
+	}
 	case 0xE000:
 		switch (opCode & 0x00F0)
 		{
 		case 0x0090: //0xEX9E: Skips the next instruction if the key stored in VX is pressed
-			Word x = (opCode << 8) & 0x000F;
-			if (key[m_VRegisters[x]] == 1)
+		{
+
+			Word x = (opCode >> 8) & 0x000F;
+			if (m_Keys[m_VRegisters[x]] == 1)
 			{
 				m_ProgramCounter += 4;
 			}
@@ -273,9 +368,12 @@ void ExecuteOpCode(Word opCode)
 				m_ProgramCounter += 2;
 			}
 			break;
+		}
 		case 0x00A0: //0xEXA1: Skips the next instruction if the key stored in VX is not pressed
-			Word x = (opCode << 8) & 0x000F;
-			if (key[m_VRegisters[x]] == 0)
+			
+		{
+			Word x = (opCode >> 8) & 0x000F;
+			if (m_Keys[m_VRegisters[x]] == 0)
 			{
 				m_ProgramCounter += 4;
 			}
@@ -284,7 +382,9 @@ void ExecuteOpCode(Word opCode)
 				m_ProgramCounter += 2;
 			}
 			break;
+		}
 		default:
+			printf("Unknown opcode [0x0000]: 0x%X\n", opCode);
 			break;
 		}
 		break;
@@ -292,13 +392,20 @@ void ExecuteOpCode(Word opCode)
 		switch (opCode & 0x00FF) // 0xFX__
 		{
 		case 0x0007: //Sets VX to the value of the delay timer
-			Word x = (opCode << 8) & 0x000F;
+
+		{
+
+			Word x = (opCode >> 8) & 0x000F;
 			m_VRegisters[x] = delayTimer;
+			m_ProgramCounter += 2;
 			break;
+		}
 		case 0x000A: //A key press is awaited, and then stored in VX
-			Word x = (opCode << 8) & 0x000F;
-			auto check = std::find(key.begin(), key.end(), 1);
-			if (check != key.end())
+		{
+
+			Word x = (opCode >> 8) & 0x000F;
+			auto check = std::find(m_Keys.begin(), m_Keys.end(), 1);
+			if (check != m_Keys.end())
 			{
 				m_VRegisters[x] = *check;
 			}
@@ -308,49 +415,257 @@ void ExecuteOpCode(Word opCode)
 			}
 			m_ProgramCounter += 2;
 			break;
+		}
+
 		case 0x0015: // Sets the delay timer to VX
+		{
+			Word x = (opCode >> 8) & 0x000F;
+			delayTimer = m_VRegisters[x];
+			m_ProgramCounter += 2;
 			break;
+		}
 		case 0x0018: // Sets the sound timer to VX
+		{
+			Word x = (opCode >> 8) & 0x000F;
+			soundTimer= m_VRegisters[x];
+			m_ProgramCounter += 2;
 			break;
+		}
+
 		case 0x001E: // Adds VX to I. VF is not affected
+		{
+			Word x = (opCode >> 8) & 0x000F;
+			if (x + m_AddressI > 0xFFF)
+			{
+				m_VRegisters[0xF] = 1;
+			}
+			else m_VRegisters[0xF] = 0;
+			m_AddressI += m_VRegisters[x];
+			m_ProgramCounter += 2;
 			break;
+		}
+
 		case 0x0029: //Sets I to the location of the sprite for the character in VX
+		{
+			Word x = (opCode >> 8) & 0x000F;
+			m_AddressI = m_VRegisters[x] * 0x5;
+			m_ProgramCounter += 2;
 			break;
+		}
 		case 0x0033: // Stores the binary-coded decimal representation of VX, with the hundreds digit in memory at location in I, the tens digit at location I+1, 
 					 //and the ones digit at location I+2
+		{
+			Word x = (opCode >> 8) & 0x000F;
+			m_Memory[m_AddressI] = (m_VRegisters[x] / 100);
+			m_Memory[m_AddressI + 1] = (m_VRegisters[x] / 10) % 10;
+			m_Memory[m_AddressI + 2] = (m_VRegisters[x] % 100) % 10;
+			m_ProgramCounter += 2;
 			break;
+		}
 		case 0x0055: //Stores from V0 to VX (including VX) 
 					 //in memory, starting at address I. The offset from I is increased by 1 for each value written, 
 					 //but I itself is left unmodified
+		{
+
+			Word x = (opCode >> 8) & 0x000F;
+			for (int i = 0; i <= x; i++)
+			{
+				m_Memory[m_AddressI + i] = m_VRegisters[i];
+			}
+			m_ProgramCounter += 2;
 			break;
+		}
 		case 0x0065: //Fills from V0 to VX (including VX) with values from 
 					 //memory, starting at address I. The offset from I is increased by 1 for each value read, 
 					 //but I itself is left unmodified
+		{
+			Word x = (opCode >> 8) & 0x000F;
+			for (int i = 0; i <= x; ++i)
+				m_VRegisters[i] = m_Memory[m_AddressI + i];
+			m_ProgramCounter += 2;
 			break;
+		}
 		default:
+			printf("Unknown opcode [0x0000]: 0x%X\n", opCode);
 			break;
 		}
 		break;
 	default:
+		printf("Unknown opcode [0x0000]: 0x%X\n", opCode);
 		break;
 	}
 }
-int main()
+#define DRAWWITHTEXTURE
+typedef unsigned __int8 u8;
+u8 screenData[SCREEN_H][SCREEN_W][3];
+// Setup Texture
+void setupTexture()
+{
+	// Clear screen
+	for (int y = 0; y < SCREEN_H; ++y)
+		for (int x = 0; x < SCREEN_W; ++x)
+		 screenData[y][x][0] = screenData[y][x][1] = screenData[y][x][2] = 0;
+
+	// Create a texture 
+	glTexImage2D(GL_TEXTURE_2D, 0, 3, SCREEN_W, SCREEN_H, 0, GL_RGB, GL_UNSIGNED_BYTE, (GLvoid*)screenData);
+
+	// Set up the texture
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+
+	// Enable textures
+	glEnable(GL_TEXTURE_2D);
+}
+
+
+void reshape_window(GLsizei w, GLsizei h)
+{
+	glClearColor(0.0f, 0.0f, 0.5f, 0.0f);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0, w, h, 0);
+	glMatrixMode(GL_MODELVIEW);
+	glViewport(0, 0, w, h);
+	// Resize quad
+	display_width = w;
+	display_height = h;
+
+
+}
+
+void updateTexture()
+{
+	// Update pixels
+	for (int y = 0; y < 32; ++y)
+		for (int x = 0; x < 64; ++x)
+			if (m_ScreenData[(y * 64) + x] == 0)
+				screenData[y][x][0] = screenData[y][x][1] = screenData[y][x][2] = 0;	// Disabled
+			else
+				screenData[y][x][0] = screenData[y][x][1] = screenData[y][x][2] = 255;  // Enabled
+
+	// Update Texture
+	glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, SCREEN_W, SCREEN_H, GL_RGB, GL_UNSIGNED_BYTE, (GLvoid*)screenData);
+
+	glBegin(GL_QUADS);
+	glTexCoord2d(0.0, 0.0);		glVertex2d(0.0, 0.0);
+	glTexCoord2d(1.0, 0.0); 	glVertex2d(display_width, 0.0);
+	glTexCoord2d(1.0, 1.0); 	glVertex2d(display_width, display_height);
+	glTexCoord2d(0.0, 1.0); 	glVertex2d(0.0, display_height);
+	glEnd();
+}
+
+void emulate()
+{
+	auto code = GetOpCode(m_Memory[m_ProgramCounter], m_Memory[m_ProgramCounter+1]);
+	ExecuteOpCode(code);
+}
+
+void display()
+{
+	emulate();
+
+	if (m_UpdateScreen)
+	{
+		// Clear framebuffer
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		updateTexture();
+
+		// Swap buffers!
+		glutSwapBuffers();
+
+		// Processed frame
+		m_UpdateScreen = false;
+	}
+}
+
+
+void keyboardDown(unsigned char key, int x, int y)
+{
+	if (key == 27)    // esc
+		exit(0);
+
+	if (key == '1')			m_Keys[0x1] = 1;
+	else if (key == '2')	m_Keys[0x2] = 1;
+	else if (key == '3')	m_Keys[0x3] = 1;
+	else if (key == '4')	m_Keys[0xC] = 1;
+
+	else if (key == 'q')	m_Keys[0x4] = 1;
+	else if (key == 'w')	m_Keys[0x5] = 1;
+	else if (key == 'e')	m_Keys[0x6] = 1;
+	else if (key == 'r')	m_Keys[0xD] = 1;
+
+	else if (key == 'a')	m_Keys[0x7] = 1;
+	else if (key == 's')	m_Keys[0x8] = 1;
+	else if (key == 'd')	m_Keys[0x9] = 1;
+	else if (key == 'f')	m_Keys[0xE] = 1;
+
+	else if (key == 'z')	m_Keys[0xA] = 1;
+	else if (key == 'x')	m_Keys[0x0] = 1;
+	else if (key == 'c')	m_Keys[0xB] = 1;
+	else if (key == 'v')	m_Keys[0xF] = 1;
+
+	//printf("Press key %c\n", key);
+}
+
+void keyboardUp(unsigned char key, int x, int y)
+{
+	if (key == '1')			m_Keys[0x1] = 0;
+	else if (key == '2')	m_Keys[0x2] = 0;
+	else if (key == '3')	m_Keys[0x3] = 0;
+	else if (key == '4')	m_Keys[0xC] = 0;
+	else if (key == 'q')	m_Keys[0x4] = 0;
+	else if (key == 'w')	m_Keys[0x5] = 0;
+	else if (key == 'e')	m_Keys[0x6] = 0;
+	else if (key == 'r')	m_Keys[0xD] = 0;
+	else if (key == 'a')	m_Keys[0x7] = 0;
+	else if (key == 's')	m_Keys[0x8] = 0;
+	else if (key == 'd')	m_Keys[0x9] = 0;
+	else if (key == 'f')	m_Keys[0xE] = 0;
+	else if (key == 'z')	m_Keys[0xA] = 0;
+	else if (key == 'x')	m_Keys[0x0] = 0;
+	else if (key == 'c')	m_Keys[0xB] = 0;
+	else if (key == 'v')	m_Keys[0xF] = 0;
+}
+
+int main(int argc, char** argv)
 {
 	srand(time(NULL));
 	m_AddressI = 0;
 	// the game loads into this address
 	m_ProgramCounter = 0x200;
 	m_VRegisters.fill(0);
+	m_ScreenData.fill(0);
+	m_Memory.fill(0);
+	m_Keys.fill(0);
+	// Load fontset
+	for (int i = 0; i < 80; ++i)
+		m_Memory[i] = Fontset[i];
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+
+	glutInitWindowSize(display_width, display_height);
+	glutInitWindowPosition(320, 320);
+	glutCreateWindow("myChip8 by Laurence Muller");
+
+	glutReshapeFunc(reshape_window);
+	glutDisplayFunc(display);
+	glutIdleFunc(display);
+	glutKeyboardFunc(keyboardDown);
+	glutKeyboardUpFunc(keyboardUp);
+	setupTexture();
 	// working directory is a project directory
-	std::fstream file{ "./../games/Soccer.ch8" };
+	std::ifstream file;
+	file.open("./../games/pong2.c8", std::ios::binary);
 
 	if (file.is_open())
 	{
 		file.seekg(0, std::ios::end);
 		auto fileSize = file.tellg();
 		file.seekg(0, std::ios::beg);
-		auto memory = (char*)m_Memory.data();
+		auto memory = (char*)m_Memory.data() + 512;
 		file.read(memory, fileSize);
 		file.close();
 	}
@@ -358,4 +673,7 @@ int main()
 	{
 		assert(false);
 	}
+
+	glutMainLoop();
+
 }
